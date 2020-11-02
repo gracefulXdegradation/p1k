eb19d756a86a59d4b8c5433d1b88b394ab5a36ec
Sasha Grinevich <alexandr.grinevich@gmail.com>
2020-11-02 22:48:25 +0200

    feat: auth service without db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://backend:kybRcKFgouGzuNkX@p1k-1.hrdvj.mongodb.net/p1k', { useNewUrlParser: true }),
+    UsersModule, AuthModule,],
   controllers: [AppController],
   providers: [AppService],
 })
